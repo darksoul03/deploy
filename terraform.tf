f11d9257646f4ecff41eab0a6563ac1c976f896a
DarkSoul <69436445+darksoul03@users.noreply.github.com>
2020-09-11 00:06:33 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -15,10 +15,21 @@ resource "aws_instance" "prod_b" {
     Name = "Prod B"
   }
 }
+resource "aws_instance" "dev" {
+   ami = "ami-0bcc094591f354be2"
+   instance_type = "t2.micro"
+   key_name = "Essential_Key_Pair_AWS"
+   tags = {
+    Name = "dev"
+  }
+}
 
 output "proda_ip"{
   value = aws_instance.prod_a.public_ip
 }
 output "prodb_ip"{
   value = aws_instance.prod_b.public_ip
+}
+output "dev_ip"{
+  value = aws_instance.dev.public_ip
 }
